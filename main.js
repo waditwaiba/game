aa7f64fddf74cfd0bda5abe3aaf0450762de797a
sajinafathima <waditwaibasajina@gmail.com>
2024-03-06 11:45:55 +0530

    working on game

--- main.js
+++ main.js
@@ -2,14 +2,8 @@ import kaboom from "kaboom";
 kaboom();
 loadSprite("ball", "/ball.png");
 let ballSize = { width: 100, height: 100 };
-let player = add([sprite("ball", ballSize), pos(center())]);
+let player = add([sprite("ball", ballSize), pos(0, center().y)]);
 const SPEED = 320;
-onKeyDown("left", () => {
-  player.move(-SPEED, 0);
-});
-onKeyDown("right", () => {
-  player.move(SPEED, 0);
-});
 
 onKeyDown("up", () => {
   player.move(0, -SPEED);
